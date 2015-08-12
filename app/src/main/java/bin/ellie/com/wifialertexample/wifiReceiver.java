0b8019c351424bddd8e5386d0a19125ac84cbcf8
Avi Rozenblum <avieli91roz@gmail.com>
2015-08-12 13:02:02 -0400

    Note the user when Wifi has disconnected

---
+++ app/src/main/java/bin/ellie/com/wifialertexample/wifiReceiver.java
@@ -0,0 +1,57 @@
+package bin.ellie.com.wifialertexample;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+public class wifiReceiver extends BroadcastReceiver {
+
+    /*
+    * This class extends BroadcastReceiver and gets broadcast when network connectivity has changed.
+    * When the change is that the user run out router range or for any reason the Wifi has disconnected,
+    * The user will be notice.
+    * Created by Avi Rozenblum
+     */
+
+    public wifiReceiver() {
+    	
+    }
+
+    // Return true if wifi connected after the connectivity changed (which means that the user turn on wifi).
+    // Parameters: Context - to get connectivity system service
+    public boolean IsWiFiConnected(Context context) {
+    	ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    	NetworkInfo ni = cm.getActiveNetworkInfo();
+    	if( ni != null && ni.getType() == ConnectivityManager.TYPE_WIFI) {    	
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+
+    // Return true if network(3G) connected after the connectivity changed (which means that the user turn off wifi).
+    // Parameters: Context - to get connectivity system service
+    public boolean isNetConnected(Context context) {
+    	ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    	NetworkInfo ni = cm.getActiveNetworkInfo();
+    	if( ni != null && ni.getType() == ConnectivityManager.TYPE_MOBILE) {    	
+    		return true;
+    	} else {
+    		return false;
+    	}     
+    }
+
+    // Check if wifi got disconnected and print toast to user.
+    public void onReceive(Context context, Intent intent) {
+        String s = intent.getAction();
+        if (s.equals("android.net.conn.CONNECTIVITY_CHANGE")) {
+            if (!IsWiFiConnected(context) && !isNetConnected(context)) {
+                Toast.makeText(context, "wifi not connected", Toast.LENGTH_LONG).show();
+            }
+            return;
+        }
+    }
+}
